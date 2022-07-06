a71e88347bfc4e85e1b40ae14f8f6417eec0dadc
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-07-06 16:40:13 +0700

    Added Joblist

--- src/Components/JobCard/JobCard.jsx
+++ src/Components/JobCard/JobCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import jobListDummy from '../../helper/jobListDummy'
 import { LocationOnOutlined, PaidOutlined, WorkOutlineOutlined } from '@mui/icons-material';
 
